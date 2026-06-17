85dcfb4e3681ff526b6dee521875c13d94693f4c
Abdiel32A <alexreve0912@gmail.com>
2026-06-17 15:09:36 -0400

    Implementacion del autoload

---
+++ vendor/autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Autoload provisional hasta ejecutar `composer install` en este entorno.
+ * No carga dependencias ni clases PSR-4; evita el error fatal de require en index.php.
+ */
